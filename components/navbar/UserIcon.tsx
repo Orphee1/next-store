e8299dbd1da582f38ea6f2cade501c5bae50c7b9
hugo lattard <hugolattard@Host-002.lan>
2024-12-05 16:50:09 +0100

    add auth provider

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { LuUser } from 'react-icons/lu'
+import { currentUser } from '@clerk/nextjs/server'
 
-function UserIcon() {
-  return <div>User Icon</div>
+async function UserIcon() {
+  const user = await currentUser()
+  const profileImage = user?.imageUrl
+
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        alt='User image'
+        className='w-6 h-6 rounded-full object-cover'
+      />
+    )
+  }
+
+  return <LuUser className='w-6 h-6 rounded-full bg-primary text-white' />
 }
 
 export default UserIcon
